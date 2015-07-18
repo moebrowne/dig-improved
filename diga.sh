2d5c3429176b437c17f89a1941662c331c987fdc
MoeBrowne <oliver.browne@uplyme.com>
2015-07-18 15:53:12 +0100

    Added some comments

--- diga.sh
+++ diga.sh
@@ -8,15 +8,20 @@ declare -A records
 recordID=0
 tableData=""
 
+# Loop through each of the records returned from dig
 while read -r record; do
 
+	# Match this record using REGEX
 	regexDNSRecord="^([[:alnum:]\-\.]+)[[:space:]]([0-9]+)[[:space:]](IN)[[:space:]]([[:alpha:]]+)[[:space:]](.+)$"
 	[[ $record =~ $regexDNSRecord ]]
 
+	# Add this records full details to the records associative array
 	records[$recordID]="${BASH_REMATCH[0]}"
 
+	# Add this records ID to the record types associative array
 	recordTypes["${BASH_REMATCH[4]}"]+="$recordID "
 
+	# Increment the record counter
 	recordID=$((recordID+1))
 
 done <<< "$DIGOUTPUT"
